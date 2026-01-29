decab3615c7a49dff54711a54a8858f94331aed8
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2026-01-29 15:45:55 +0800

    fix: remove retry button and update progress display logic

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 import useFetchData from "../hooks/useFetchData";
 
 export default function Loading() {
-  const { retry, progress } = useFetchData();
+  const { progress } = useFetchData();
 
   const totalProgress =
     Math.round(
@@ -13,13 +13,11 @@ export default function Loading() {
         <div className="flex flex-col items-center h-auto gap-2">
           <div className="flex items-center gap-1 justify-center">
             <p>{progress.message}</p>
-            {progress.code !== 500 && <p>{totalProgress}</p>}
+            {progress.books !== 100 &&
+              progress.vocabulary !== 100 &&
+              progress.wordBookMappings !== 100 &&
+              progress.examples !== 100 && <p>{totalProgress}</p>}
           </div>
-          {progress.code === 500 && (
-            <button onClick={retry} title="retry" className="button-p">
-              重试
-            </button>
-          )}
         </div>
       </div>
     </>
